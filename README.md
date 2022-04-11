869ce4401ab4591b49bc3545e76a561ed43b0eb2
your.user.name <nfinck11@hotmail.com>
2022-04-11 12:12:01 -0500

    adding headline summary and details

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).
-=======
 # new-pork-times
 A (fake) food news organization website build using Next.JS
->>>>>>> 1a4a9624d408bad136040b87f9cc9711cf170fcc
--- pages/posts/1.jsx
+++ pages/posts/1.jsx
@@ -10,8 +10,23 @@ export default function FirstPost() {
                     <h1>
                         Companies waging battle on crops as food vs fuel
                     </h1>
-                    <div className={blogStyles.summary}>
-                    Food or fuel? Food companies are waging a battle in the fields, in laboratories and in the halls of government over how to use corn as demand for the grain and other food ingredients surges for use in ethanol and other biofuels.
+                    <div className={blogStyles.summaryContainer}>
+                        <div className={blogStyles.content}>
+                            Food or fuel? Food companies are waging a battle in the fields, in laboratories and in the halls of government over how to use corn as demand for the grain and other food ingredients surges for use in ethanol and other biofuels.
+                        </div>
+                        <div className={blogStyles.details}>
+                            <div className={blogStyles.leftDetails}>
+                                <div>
+                                    By Brad Dorfman
+                                </div>
+                                <div>
+                                    March 9, 2007
+                                </div>
+                            </div>
+                            <div className={blogStyles.rightDetails}>
+                                4 MIN READ
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
