16ff25d758d00b45145f545f5a6fe03d41962b85
your.user.name <nfinck11@hotmail.com>
2022-04-13 09:10:34 -0500

    attempting to fix svg font issue in header

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,10 @@ import Header from '../components/header'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <div>
+  return (
+  <div>
     <Header></Header>
     <Component {...pageProps} />
   </div>
-  
+  )
 }
