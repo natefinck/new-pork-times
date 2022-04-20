3c1cf429c73a46c2cb73700d35abd80bbe31951b
natefinck <nfinck11@hotmail.com>
2022-04-20 15:10:56 -0500

    adding main article section on home page, widest view only

--- components/header.jsx
+++ components/header.jsx
@@ -1,82 +1,83 @@
-import React, { useState, useEffect } from 'react';
-import styles from '../styles/header.module.scss'
-import Image from 'next/image'
-import logo from '../images/logo.png'
-import SideNavPanel from './sideNavPanel'
-import Hamburger from '../components/hamburgerMenu'
+import React, { useState, useEffect } from "react";
+import styles from "../styles/header.module.scss";
+import Image from "next/image";
+import logo from "../images/logo.png";
+import SideNavPanel from "./sideNavPanel";
+import Hamburger from "../components/hamburgerMenu";
 import onClickOutside from "react-onclickoutside";
-import Link from 'next/link'
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 function Header() {
-    const [menuOpen, setMenuOpen] = useState(false);
-    const [home, setHome] = useState(true);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [home, setHome] = useState(true);
 
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    }
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
-    Header.handleClickOutside = () => {
-        setMenuOpen(false);
+  Header.handleClickOutside = () => {
+    setMenuOpen(false);
+  };
+
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      if (url === "/") setHome(true);
+      else setHome(false);
     };
+    router.events.on("routeChangeStart", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange);
+    };
+  }, []);
 
-    const router = useRouter()
-    useEffect(() => {
-      const handleRouteChange = (url) => {
-        if (url === '/') setHome(true)
-        else setHome(false);
-      }
-      router.events.on('routeChangeStart', handleRouteChange)
-      return () => {
-        router.events.off('routeChangeStart', handleRouteChange)
-      }
-    }, [])
+  const navLinks = [
+    { displayText: "Home", href: "/" },
+    { displayText: "World", href: "/" },
+    { displayText: "Business", href: "/" },
+    { displayText: "Politics", href: "/" },
+    { displayText: "U.S.", href: "/" },
+    { displayText: "Sports", href: "/" },
+    { displayText: "Health", href: "/" },
+    { displayText: "N.Y.", href: "/" },
+    { displayText: "Opinion", href: "/" },
+    { displayText: "Tech", href: "/" },
+    { displayText: "Science", href: "/" },
+    { displayText: "Art", href: "/" },
+    { displayText: "Books", href: "/" },
+    { displayText: "Style", href: "/" },
+    { displayText: "Food", href: "/" },
+    { displayText: "Travel", href: "/" },
+    { displayText: "Magazine", href: "/" },
+    { displayText: "Real Estate", href: "/" },
+    { displayText: "Obituaries", href: "/" },
+  ];
 
-    const navLinks = [
-    {'displayText': 'Home', 'href': '/'},
-    {'displayText': 'World', 'href': '/'},
-    {'displayText': 'Business', 'href': '/'},
-    {'displayText': 'Politics', 'href': '/'},
-    {'displayText': 'U.S.', 'href': '/'},
-    {'displayText': 'Sports', 'href': '/'},
-    {'displayText': 'Health', 'href': '/'},
-    {'displayText': 'N.Y.', 'href': '/'},
-    {'displayText': 'Opinion', 'href': '/'},
-    {'displayText': 'Tech', 'href': '/'},
-    {'displayText': 'Science', 'href': '/'},
-    {'displayText': 'Art', 'href': '/'},
-    {'displayText': 'Books', 'href': '/'},
-    {'displayText': 'Style', 'href': '/'},
-    {'displayText': 'Food', 'href': '/'},
-    {'displayText': 'Travel', 'href': '/'},
-    {'displayText': 'Magazine', 'href': '/'},
-    {'displayText': 'Real Estate', 'href': '/'},
-    {'displayText': 'Obituaries', 'href': '/'}
-]
-
-    return (
-        <>
-            <div className={styles.headerContainer}>
-                <div className={`${styles.header} header`}>
-                    <Link href="/">
-                        <div className={`${styles.logoContainer} logoContainer`}>
-                            <Image
-                                alt="The New Pork Times Logo"
-                                src={logo}>
-                            </Image>
-                        </div>
-                    </Link>
-                    <div className={`${styles.navContainer} navContainer`} onClick={() => setMenuOpen(false)}>
-                        <SideNavPanel navLinks={navLinks}/>
-                    </div>
-                    <div
-                    className={styles.hamburger}
-                    onClick={() => {
-                            toggleMenu(true)
-                    }}>
-                        <Hamburger menuopen={menuOpen}/>
-                    </div>
-                    {/* <div className={`${styles.bottomBar} bottomBar`}>
+  return (
+    <>
+      <div className={styles.headerContainer}>
+        <div className={`${styles.header} header`}>
+          <Link href="/">
+            <div className={`${styles.logoContainer} logoContainer`}>
+              <Image alt="The New Pork Times Logo" src={logo}></Image>
+            </div>
+          </Link>
+          <div
+            className={`${styles.navContainer} navContainer`}
+            onClick={() => setMenuOpen(false)}
+          >
+            <SideNavPanel navLinks={navLinks} />
+          </div>
+          <div
+            className={styles.hamburger}
+            onClick={() => {
+              toggleMenu(true);
+            }}
+          >
+            <Hamburger menuopen={menuOpen} />
+          </div>
+          {/* <div className={`${styles.bottomBar} bottomBar`}>
                         <ul>
                             {navLinks.map((l, i) => 
                                 <li key={l.displayText}>
@@ -87,47 +88,51 @@ function Header() {
                             )}
                         </ul>
                     </div> */}
-                </div>
-                <div className={`${styles.bottomNavBorder} bottomNavBorder`} />
-            </div>
-            <style jsx>{`
-                .navContainer {
-                    box-shadow: ${menuOpen ? '0px 0px 16px 2px black' : '0px 0px 0px 0px black'};
-                    transform: ${menuOpen ? 'translateX(0%)' : 'translateX(-100%)'};
-                    transition: ${menuOpen ? 'all .3s cubic-bezier(0, 0.65, 0.58, 1)' : 'all .25s cubic-bezier(0.68, -0.06, 0.48, 0.99)'};
-                }
+        </div>
+        <div className={`${styles.bottomNavBorder} bottomNavBorder`} />
+      </div>
+      <style jsx>{`
+        .navContainer {
+          box-shadow: ${menuOpen
+            ? "0px 0px 16px 2px black"
+            : "0px 0px 0px 0px black"};
+          transform: ${menuOpen ? "translateX(0%)" : "translateX(-100%)"};
+          transition: ${menuOpen
+            ? "all .3s cubic-bezier(0, 0.65, 0.58, 1)"
+            : "all .25s cubic-bezier(0.68, -0.06, 0.48, 0.99)"};
+        }
 
-                .header {
-                    padding-bottom: ${home ? 'intial' : '.3rem'}
-                }
+        .header {
+          padding-bottom: ${home ? "intial" : ".3rem"};
+        }
 
-                .logoContainer {
-                    width: ${home ? '40%' : '30%'};
-                }
+        .logoContainer {
+          width: ${home ? "40%" : "30%"};
+        }
 
-                .bottomBar {
-                    height: ${home ? '40px' : '0px'};
-                }
+        .bottomBar {
+          height: ${home ? "40px" : "0px"};
+        }
 
-                .bottomNavBorder {
-                    opacity: ${home ? '1' : '0'};
-                    height: ${home ? '4px' : '0px'}
-                    // border-top: ${home ? '1px solid var(--main-black)' : 0};
-                    // border-bottom: ${home ? '1px solid var(--main-black)' : 0};
-                }
+        .bottomNavBorder {
+          opacity: ${home ? "1" : "0"};
+          height: ${home ? "4px" : "0px"};
+          // border-top: ${home ? "1px solid var(--main-black)" : 0};
+          // border-bottom: ${home ? "1px solid var(--main-black)" : 0};
+        }
 
-                @media only screen and (max-width: 767px) {
-                    .logoContainer {
-                        width: ${home ? '60%' : '50%'};
-                    }
-                }
-            `}</style>
-        </>
-    )
+        @media only screen and (max-width: 767px) {
+          .logoContainer {
+            width: ${home ? "60%" : "50%"};
+          }
+        }
+      `}</style>
+    </>
+  );
 }
 
 const clickOutsideConfig = {
-    handleClickOutside: () => Header.handleClickOutside,
-  };
+  handleClickOutside: () => Header.handleClickOutside,
+};
 
 export default onClickOutside(Header, clickOutsideConfig);
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "../styles/index.module.scss";
+import poke from "../images/poke.jpeg";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -12,7 +14,63 @@ export default function Home() {
           <div className={`${styles.mainTitle} mainTitle`}>
             <h1>Ukraine and Russia Trade Strikes Along Eastern Front</h1>
           </div>
-          <div className={`${styles.mainContainer} mainContainer`}></div>
+          <div className={`${styles.mainContent} mainContent`}>
+            <div className={`${styles.main1h} main1h`}>
+              <div className={`${styles.main1htitle} main1htitle`}>
+                <h1>
+                  Ukrainian Forces in Mariupol Appeal for Help in Last Stand
+                </h1>
+              </div>
+              <div className={`${styles.main1hul} main1hul`}>
+                <ul className={`${styles.subtext}`}>
+                  <li>
+                    The two sides traded artillery barrages as Russia tried to
+                    pierce the defenses of dug-in Ukrainian fighters.
+                  </li>
+                  <li>
+                    A tentative deal was reached to evacuate some women and
+                    children from the besieged city of Mariupol, but such
+                    agreements have fallen through in the past. Here's the
+                    latest.
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className={`${styles.main2h} ${styles.borderBottom} main2h`}>
+              <div className={`${styles.main2htitle} main2htitle`}>
+                <h2>
+                  The Race to Arm Ukraine With Advanced Weapons Brings a Notable
+                  Risk
+                </h2>
+              </div>
+              <div
+                className={`${styles.main2hcontent} ${styles.subtext} main2hcontent`}
+              >
+                The U.S. and its allies are sending longer-range weapons like
+                howitzers and antiaircraft systems. Russia has said that such
+                actions could ignite a wider war.
+              </div>
+            </div>
+            <div className={`${styles.main3h} ${styles.main3h1} main3h`}>
+              <div className={`${styles.main3htitle} main3htitle`}>
+                <h3>
+                  Civilians who escaped the besieged steel plant in Mariupol
+                  described a harrowing life underground.
+                </h3>
+              </div>
+            </div>
+            {/* <div className={`${styles.main3h} ${styles.main3h2} main3h`}>
+              <div className={`${styles.main3htitle} main3htitle`}>
+                <h3>
+                  Under Russian bombardment, a Ukrainian commander in Mariupol
+                  said that his forces may have only hours left.
+                </h3>
+              </div>
+            </div> */}
+            <div className={`${styles.main1himage} main1himage`}>
+              <Image alt="Image of Poke" src={poke}></Image>
+            </div>
+          </div>
         </div>
       </main>
     </div>
