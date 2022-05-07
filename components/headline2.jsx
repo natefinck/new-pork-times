9644c7f09d5b6c429543857eb39975d95b5c452a
Nate Finck <nfinck11@hotmail.com>
2022-05-07 11:53:15 -0500

    more headline 2 styles and adding link to article page

--- components/headline2.jsx
+++ components/headline2.jsx
@@ -5,10 +5,9 @@ import poke from "../images/poke.jpeg";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Headline() {
-
+export default function Headline2() {
   return (
-      <>
+    <>
       <div className={styles.headlineContainer}>
         {/* <h1 className={styles.title}>This is the home page.</h1>
         <h1 className={styles.title}>Check out our first <Link href="/posts/1"><a>article</a></Link>!</h1> */}
@@ -19,87 +18,100 @@ export default function Headline() {
             <h1>Ukraine and Russia Trade Strikes Along Eastern Front</h1>
           </div> */}
           <div className={`${styles.mainContent} mainContent`}>
-            <div className={`${headline1styles.main1h} main1h`}>
+            <Link href={"/posts/1"}>
               <div
-                className={`${headline1styles.main1htitle} ${styles.articleContainer} main1htitle`}
+                className={`${headline1styles.main1h} ${styles.link} main1h`}
               >
-                <Link href={"/posts/1"}>
+                <div
+                  className={`${headline1styles.main1htitle} ${styles.articleContainer} main1htitle`}
+                >
                   <h1>
-                    Ukrainian Forces in Mariupol Appeal for Help in Last Stand
+                    Need a Big Mac Out on the Tundra? There's an App (and a
+                    Plane) for That.
                   </h1>
-                </Link>
+                </div>
+                <div className={`${headline1styles.main1hul} main1hul`}>
+                  <ul className={`${styles.subtext}`}>
+                    <li>
+                      With the help of bush pilots, residents of remote Alaskan
+                      villages are increasingly using DoorDash, Uber Eats and
+                      other food-delivery services.
+                    </li>
+                    <li>
+                      A tentative deal was reached with food-delivery services
+                      to increase food avaliability in some areas - and it's
+                      showing massive improvement.
+                    </li>
+                  </ul>
+                </div>
               </div>
-              <div className={`${headline1styles.main1hul} main1hul`}>
-                <ul className={`${styles.subtext}`}>
-                  <li>
-                    The two sides traded artillery barrages as Russia tried to
-                    pierce the defenses of dug-in Ukrainian fighters.
-                  </li>
-                  <li>
-                    A tentative deal was reached to evacuate some women and
-                    children from the besieged city of Mariupol, but such
-                    agreements have fallen through in the past. Here's the
-                    latest.
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div
-              className={`${headline1styles.main1himage} ${headline1styles.articleContainer} main1himage`}
-            >
-              <Image alt="Image of Poke" src={poke}></Image>
-            </div>
-            <div
-              className={`${headline1styles.main3h} ${headline1styles.main3h1} ${styles.borderBottom} ${styles.articleContainer} main3h`}
-            >
-              <div className={`${headline1styles.main3htitle} main3htitle`}>
-                <h3>
-                  Civilians who escaped the besieged steel plant in Mariupol
-                  described a harrowing life underground.
-                </h3>
-              </div>
-            </div>
-            <div
-              className={`${headline1styles.main2h} ${styles.articleContainer} ${headline1styles.main2h1} ${styles.borderBottom} main2h`}
-            >
-              <div className={`${headline1styles.main2htitle} main2htitle`}>
-                <h2>Best Salmon Recipes for Weeknight Dinners</h2>
-              </div>
+            </Link>
+            <Link href={"/posts/1"}>
               <div
-                className={`${headline1styles.main2hcontent} ${styles.subtext} main2hcontent`}
+                className={`${headline1styles.main1himage} ${styles.link} ${headline1styles.articleContainer} main1himage`}
               >
-                Baked in the oven or pan-fried on the stovetop, these are our
-                best salmon recipes for an easy weeknight dinner.
+                <Image alt="Image of Poke" src={poke}></Image>
               </div>
-            </div>
-            <div
-              className={`${headline1styles.main2h} ${styles.articleContainer} ${headline1styles.main2h2} main2h`}
-            >
-              <div className={`${headline1styles.main2htitle} main2htitle`}>
-                <h2>The Pour - 12 Natural Wines to Drink Now</h2>
-              </div>
+            </Link>
+            <Link href={"/posts/1"}>
               <div
-                className={`${headline1styles.main2hcontent} ${styles.subtext} main2hcontent`}
+                className={`${headline1styles.main3h} ${styles.link} ${headline1styles.main3h1} ${styles.borderBottom} ${styles.articleContainer} main3h`}
               >
-                For 20 years, natural wine has been dismissed as a fad or a
-                fraud. Yet more people make it, more drink it and good bottles
-                are easier to find. Some are curious because it's fashionable.
-                Others are curious about wines made outside the norm
+                <div className={`${headline1styles.main3htitle} main3htitle`}>
+                  <h3>
+                    Civilians who escaped the besieged steel plant in Mariupol
+                    described a harrowing life underground.
+                  </h3>
+                </div>
               </div>
-            </div>
-            <div
-              className={`${headline1styles.main3h} ${styles.articleContainer} ${headline1styles.main3h2} main3h`}
-            >
-              <div className={`${headline1styles.main3htitle} main3htitle`}>
-                <h3>
-                  Under Russian bombardment, a Ukrainian commander in Mariupol
-                  said that his forces may have only hours left.
-                </h3>
+            </Link>
+            <Link href={"/posts/1"}>
+              <div
+                className={`${headline1styles.main2h} ${styles.link} ${styles.articleContainer} ${headline1styles.main2h1} ${styles.borderBottom} main2h`}
+              >
+                <div className={`${headline1styles.main2htitle} main2htitle`}>
+                  <h2>Best Salmon Recipes for Weeknight Dinners</h2>
+                </div>
+                <div
+                  className={`${headline1styles.main2hcontent} ${styles.subtext} main2hcontent`}
+                >
+                  Baked in the oven or pan-fried on the stovetop, these are our
+                  best salmon recipes for an easy weeknight dinner.
+                </div>
               </div>
-            </div>
+            </Link>
+            <Link href={"/posts/1"}>
+              <div
+                className={`${headline1styles.main2h} ${styles.link} ${styles.articleContainer} ${headline1styles.main2h2} main2h`}
+              >
+                <div className={`${headline1styles.main2htitle} main2htitle`}>
+                  <h2>The Pour - 12 Natural Wines to Drink Now</h2>
+                </div>
+                <div
+                  className={`${headline1styles.main2hcontent} ${styles.subtext} main2hcontent`}
+                >
+                  For 20 years, natural wine has been dismissed as a fad or a
+                  fraud. Yet more people make it, more drink it and good bottles
+                  are easier to find. Some are curious because it's fashionable.
+                  Others are curious about wines made outside the norm
+                </div>
+              </div>
+            </Link>
+            <Link href={"/posts/1"}>
+              <div
+                className={`${headline1styles.main3h} ${styles.link} ${styles.articleContainer} ${headline1styles.main3h2} main3h`}
+              >
+                <div className={`${headline1styles.main3htitle} main3htitle`}>
+                  <h3>
+                    Under Russian bombardment, a Ukrainian commander in Mariupol
+                    said that his forces may have only hours left.
+                  </h3>
+                </div>
+              </div>
+            </Link>
           </div>
         </div>
       </div>
-      </>
+    </>
   );
 }
